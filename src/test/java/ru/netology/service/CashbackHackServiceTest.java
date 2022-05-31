3ba4caadda46f600fddda9917cdf5e88e6da0af1
Александр Сиденов <sid_san@mail.ru>
2022-05-31 17:52:29 +0800

    add branch TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldReturnIfaAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldReturnIfaAmountIs1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldReturnIfaAmountIs999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void shouldReturnIfaAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual,expected);
+    }
+}
